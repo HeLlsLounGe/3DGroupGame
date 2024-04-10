bbe40c815816ceace0fc6503988b59a35e2e6cca
Nicky-The-Squiggler <nicky.werp@yahoo.com>
2024-04-10 09:37:55 -0700

    script is being agravating

--- 3d Group Project/Assets/Scripts/EnemyAttackScript.cs
+++ 3d Group Project/Assets/Scripts/EnemyAttackScript.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class EnemyAttackScript : MonoBehaviour
 {
-    public int warnings = 4;
-    public float warningRate = 0.2;
-    void Start()
+    public int warnings = 3;
+    private int currentWarning;
+    public float warningRate = 0.2f;
+    private float warningRateTimer;
+    [SerializeField] AudioClip warningSound;
+    [SerializeField] AudioClip fire;
+
+
+   void Start()
     {
-        
+        currentWarning = warnings;
     }
 
-  void Attack()
+  public void WindUp()
     {
+        if (currentWarning > 0)
+        { Invoke(nameof(Sound)); }
 
+        else if (currentWarning <= 0)
+        { Invoke(nameof(Attack)); }
+    }
+  void Sound()
+    {
+        //warningSound.Play();
+        currentWarning--;
+       Invoke(nameof(WindUp), warningRate); 
+    }
+
+    void Attack()
+    {
+       // fire.Play();
+        currentWarning = warnings;
     }
 }
