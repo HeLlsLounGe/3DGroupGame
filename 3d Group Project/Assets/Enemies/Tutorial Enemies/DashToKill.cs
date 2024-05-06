7b222dc81ec5bac62eddde3247ebf42ddec87695
Nicky-The-Squiggler <nicky.werp@yahoo.com>
2024-05-06 08:53:27 -0700

    Completed tutorial Level

--- 3d Group Project/Assets/Enemies/Tutorial Enemies/DashToKill.cs
+++ 3d Group Project/Assets/Enemies/Tutorial Enemies/DashToKill.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class DashToKill : MonoBehaviour
 {
     EnemyHealthBar healthScript;
-    float healTime = 0.1f;
+    float healTime = 0.3f;
     float healthTimer;
     void Start()
     {
         healthScript = GetComponent<EnemyHealthBar>();
+        healthTimer = healTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (healthTimer > 0)
+        {
+            healthTimer = healthTimer - Time.deltaTime;
+        }
+        else if (healthTimer <= 0)
+        {
+            healthTimer = healTime;
+            if (healthScript.hitpoints > 0)
+            {
+                healthScript.hitpoints = healthScript.MaxHP;
+            }
+        }
     }
 }
